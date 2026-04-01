c36e06ca14cb2dc8318cb9142a5decbb230a12ee
anggavb <anggavb8@gmail.com>
2026-04-01 19:35:01 +0700

    chore: remove all task from dom2

--- js/minitask-1.js
+++
@@ -1,8 +0,0 @@
-const password = document.querySelector("#password");
-const toggle = document.querySelector("#togglePassword");
-
-toggle.addEventListener("click", () => {
-    const type = password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
-    toggle.textContent = type === "password" ? "👁️" : "🙈";
-});
--- js/minitask-2.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const dropdownMenu = document.querySelector('.dropdown-menu');
-
-menuToggle.addEventListener('click', () => {
-  dropdownMenu.classList.toggle('show');
-});
